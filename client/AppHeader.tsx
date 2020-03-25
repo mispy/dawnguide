02aa37fa87a94934869d21ebc6bbf8aab5ffe2a3
Jaiden Mispy <jaiden@mispy.me>
2020-03-25 12:47:34 +1100

    Mobile compatibility tweaks

--- client/AppHeader.tsx
+++ client/AppHeader.tsx
@@ -5,38 +5,36 @@ import { Link } from "react-router-dom"
 import { AppContext } from "./AppContext"
 
 export function AppHeader() {
-  const { store } = useContext(AppContext)
-  return <header className="AppHeader">
-    <Navbar>
-      <Container>
-        <div>
-          <Navbar.Brand href="/home">Sunpeep</Navbar.Brand>
-          <span className="environment">alpha</span>
-        </div>
-        <Nav className="learnButtons">
-          <ul className="navigation-shortcuts">
-            <li className="navigation-shortcut navigation-shortcut--lessons">
-              <Link to="/lesson">
-                <span className={store.numLessons === 0 ? 'inactive' : undefined}>{store.numLessons}</span> Lessons
-                                    </Link>
-            </li>
-            <li className="navigation-shortcut navigation-shortcut--reviews">
-              <Link to="/review">
-                <span className={store.numReviews === 0 ? 'inactive' : undefined}>{store.numReviews}</span> Reviews
+    const { store } = useContext(AppContext)
+    return <header className="AppHeader">
+        <Navbar>
+            <Container>
+                <div>
+                    <Navbar.Brand href="/home">Sunpeep</Navbar.Brand>
+                    <span className="environment">alpha</span>
+                </div>
+                <div className="ml-auto d-flex align-items-center">
+                    <Nav className="learnButtons">
+                        <ul className="navigation-shortcuts">
+                            <li className="navigation-shortcut navigation-shortcut--lessons">
+                                <Link to="/lesson">
+                                    <span className={store.numLessons === 0 ? 'inactive' : undefined}>{store.numLessons}</span> Lessons
                                 </Link>
-            </li>
-          </ul>
-        </Nav>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          {/* <Nav onClick={() => setShowAbout(true)}>
-                            <Nav.Link>About</Nav.Link>
-                        </Nav> */}
-          <Nav>
-            <Nav.Link href="/logout">Logout</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-  </header>
+                            </li>
+                            <li className="navigation-shortcut navigation-shortcut--reviews">
+                                <Link to="/review">
+                                    <span className={store.numReviews === 0 ? 'inactive' : undefined}>{store.numReviews}</span> Reviews
+                            </Link>
+                            </li>
+                        </ul>
+                    </Nav>
+                    <Nav className="other">
+                        <Nav.Link href="/logout">Logout</Nav.Link>
+                    </Nav>
+
+                </div>
+
+            </Container>
+        </Navbar>
+    </header>
 }
