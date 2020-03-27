86d82abc5d679f3c81bfb9352ee9db51f92c6cc2
Jaiden Mispy <jaiden@mispy.me>
2020-03-27 16:00:17 +1100

    Loading indicator on button

--- client/SettingsPage.tsx
+++ client/SettingsPage.tsx
@@ -10,22 +10,45 @@ import { bind } from "decko"
 
 import { Container } from "react-bootstrap"
 
-class SettingsPageState {
-    @observable newEmail: string = ""
-    @observable newEmailPassword: string = ""
+function ChangeEmailSection() {
+    const { api } = useContext(AppContext)
+    const state = useLocalStore(() => ({ newEmail: "", newEmailPassword: "", loading: false }))
 
-    constructor(readonly api: SunpeepApi) { }
-
-    @bind async changeEmail(e: React.FormEvent) {
+    async function changeEmail(e: React.FormEvent) {
         e.preventDefault()
 
-        await this.api.changeEmail({ newEmail: this.newEmail, password: this.newEmailPassword })
+        runInAction(() => state.loading = true)
+        try {
+            await api.changeEmail({ newEmail: state.newEmail, password: state.newEmailPassword })
+        } finally {
+            runInAction(() => state.loading = false)
+        }
     }
+
+    return useObserver(() => <section>
+        <h2 id="email">Email</h2>
+        <p>
+            Associate a new email address to your Sunpeep account. We'll send a confirmation email to your new address. You are required to click the link in the email to finalize the change.
+        </p>
+        <form onSubmit={changeEmail}>
+            <div className="form-group">
+                <label>New email address</label>
+                <input name="email" type="email" className="form-control" placeholder="Email" required value={state.newEmail} onChange={action(e => state.newEmail = e.currentTarget.value)} />
+            </div>
+            <div className="form-group">
+                <label>Password</label>
+                <input name="password" type="password" className="form-control" placeholder="Password" required value={state.newEmailPassword} onChange={action(e => state.newEmailPassword = e.currentTarget.value)} />
+            </div>
+            <button className="btn btn-outline-secondary" type="submit">
+                {state.loading ? <span className="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span> : undefined}
+                Update email address
+            </button>
+        </form>
+    </section>)
 }
 
 export function SettingsPage() {
     const { api } = useContext(AppContext)
-    const state = useLocalStore(() => new SettingsPageState(api))
 
     useEffect(() => {
         // state.loadUsers()
@@ -35,23 +58,7 @@ export function SettingsPage() {
         <main className="SettingsPage">
             <Container>
                 <h1>Settings</h1>
-                <section>
-                    <h2 id="email">Email</h2>
-                    <p>
-                        Associate a new email address to your Sunpeep account. We'll send a confirmation email to your new address. You are required to click the link in the email to finalize the change.
-                    </p>
-                    <form onSubmit={state.changeEmail}>
-                        <div className="form-group">
-                            <label>New email address</label>
-                            <input name="email" type="email" className="form-control" placeholder="Email" required value={state.newEmail} onChange={action(e => state.newEmail = e.currentTarget.value)} />
-                        </div>
-                        <div className="form-group">
-                            <label>Password</label>
-                            <input name="password" type="password" className="form-control" placeholder="Password" required value={state.newEmailPassword} onChange={action(e => state.newEmailPassword = e.currentTarget.value)} />
-                        </div>
-                        <button className="btn btn-outline-secondary" type="submit">Update email address</button>
-                    </form>
-                </section>
+                <ChangeEmailSection />
             </Container>
         </main>
     </AppLayout>)
