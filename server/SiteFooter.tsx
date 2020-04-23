fe8c3cdee9554340126c3677662b6be6138a0be4
Jaiden Mispy <jaiden@mispy.me>
2020-04-23 11:56:20 +1000

    Here's a footer, when we have links to go in it

--- server/SiteFooter.tsx
+++ server/SiteFooter.tsx
@@ -2,5 +2,12 @@ import React = require("react")
 
 export function SiteFooter() {
     return <footer className="SiteFooter">
+        {/* <div className="container">
+            <ul>
+                <li>
+                    Copyright © Dawnlight Technology
+                </li>
+            </ul>
+        </div> */}
     </footer>
 }
