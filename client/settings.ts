2205b0caeb949d9f3ecae58502cdd63f07116549
Jaiden Mispy <jaiden@mispy.me>
2020-05-03 19:48:03 +1000

    Switch to dawnguide-specific keys

--- client/settings.ts
+++ client/settings.ts
@@ -3,7 +3,7 @@ declare const process: any
 
 export const IS_PRODUCTION = !window.location.origin.includes('localhost')
 
-export const STRIPE_PUBLIC_KEY = IS_PRODUCTION ? "pk_live_JLjSUr5RxAdaPzv4d7uBMOlw" : "pk_test_mfLIw9GyX5nCZBKawbFMJV6P"
+export const STRIPE_PUBLIC_KEY = IS_PRODUCTION ? "pk_live_PK28XpFQ6PvWfTzMljEe97ND00q1Bm5Kor" : "pk_test_V6b2H2AFMzPTPmdfF89f505q00jklfw0PJ"
 
 export const API_BASE_URL = `${window.location.origin}`
 
