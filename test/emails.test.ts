f97bc2d0f91354aaac3f7aee1846236afaa9d890
Jaiden Mispy <jaiden@mispy.me>
2020-05-09 19:43:17 +1000

    Add test for heartbeat sending emails

--- test/emails.test.ts
+++ test/emails.test.ts
@@ -2,6 +2,7 @@
 import { heartbeat } from '../server/systemController'
 import { testMailsSent } from '../server/mail'
 import * as db from '../server/db'
+import { weeks } from '../server/time'
 
 afterEach(() => {
     while (testMailsSent.length)
@@ -11,7 +12,7 @@ afterEach(() => {
 describe('emails', () => {
     it('sends lesson and review emails', async () => {
 
-        await db.users.create({
+        const user = await db.users.create({
             username: "fluffles",
             email: "fluffles@gmail.com",
             password: "imfluffles"
@@ -19,10 +20,25 @@ describe('emails', () => {
 
         await heartbeat()
 
-        expect(testMailsSent.length).toBe(1)
-        console.log(testMailsSent)
+        // Newly created user won't receive an immediate reminder email
+        expect(testMailsSent.length).toBe(0)
 
-        // expect(matchesAnswerPermissively("wafflez", "waffles")).toBe(true)
-        // expect(matchesAnswerPermissively("wafflezzz", "waffles")).toBe(false)
+
+        await db.notificationSettings.update(user.id, {
+            lastWeeklyReviewEmail: Date.now() - weeks(1)
+        })
+
+        await heartbeat()
+
+        // Now heartbeat will have sent an email
+        expect(testMailsSent.length).toBe(1)
+
+        const msg = testMailsSent[0]
+        expect(msg.to).toBe("fluffles@gmail.com")
+        expect(msg.subject).toBe("Your Lessons and Reviews Update")
+
+        // But it won't send another one yet
+        await heartbeat()
+        expect(testMailsSent.length).toBe(1)
     })
 })
