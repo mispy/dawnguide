84aa8fe5e324a65d7a775d26c2979440663215b3
Jaiden Mispy <jaiden@mispy.me>
2020-06-06 01:35:06 +1000

    Dev stuff all working with webpack

--- webpack.client.config.js
+++ webpack.client.config.js
@@ -1,46 +1,49 @@
 const path = require('path')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
-module.exports = {
-    entry: {
-        app: "./client/app.tsx",
-        site: "./client/site.sass"
-    },
-    output: {
-        filename: '[name].js',
-        path: path.resolve(__dirname, 'client/dist'),
-    },
-    resolve: {
-        extensions: ['.tsx', '.ts', '.js'],
-    },
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: 'ts-loader',
-                exclude: /node_modules/,
-            },
-            {
-                test: /\.css$/,
-                use: [MiniCssExtractPlugin.loader, 'css-loader']
-            },
-            {
-                test: /\.sass$/,
-                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
-            },
-            {
-                test: /\.(jpe?g|gif|png|eot|woff|ttf|svg|woff2)$/,
-                loader: 'url-loader?limit=10000'
-            }
+module.exports = (env, argv) => {
+    const isProduction = argv.mode === 'production'
+    return {
+        entry: {
+            app: "./client/app.tsx",
+            site: "./client/site.sass"
+        },
+        output: {
+            filename: '[name].js',
+            path: path.resolve(__dirname, 'client/dist'),
+        },
+        resolve: {
+            extensions: ['.tsx', '.ts', '.js'],
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.tsx?$/,
+                    use: 'ts-loader',
+                    exclude: /node_modules/,
+                },
+                {
+                    test: /\.css$/,
+                    use: [MiniCssExtractPlugin.loader, 'css-loader']
+                },
+                {
+                    test: /\.sass$/,
+                    use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+                },
+                {
+                    test: /\.(jpe?g|gif|png|eot|woff|ttf|svg|woff2)$/,
+                    loader: 'url-loader?limit=10000'
+                }
+            ],
+        },
+        plugins: [
+            new MiniCssExtractPlugin({
+                filename: '[name].css',
+            }),
         ],
-    },
-    plugins: [
-        new MiniCssExtractPlugin({
-            filename: '[name].css',
-        }),
-    ],
-    devServer: {
-        port: 1234,
-        contentBase: false
+        devServer: {
+            port: 1234,
+            contentBase: false
+        }
     }
-};
+}
--- webpack.server.config.js
+++ webpack.server.config.js
@@ -1,26 +1,34 @@
 const path = require('path');
 
-module.exports = {
-    target: "webworker",
-    entry: "./server/worker.tsx",
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: 'ts-loader',
-                exclude: /node_modules/,
-            },
-            {
-                test: /\.(jpe?g|gif|png|eot|woff|ttf|svg|woff2)$/,
-                loader: 'url-loader?limit=10000'
-            }
-        ],
-    },
-    resolve: {
-        extensions: ['.tsx', '.ts', '.js'],
-    },
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist'),
+module.exports = (env, argv) => {
+    const isProduction = argv.mode === 'production'
+
+    return {
+        target: "webworker",
+        mode: isProduction ? 'production' : 'development',
+        devtool: 'cheap-module-source-map',
+        entry: {
+            worker: "./server/worker.tsx"
+        },
+        output: {
+            filename: '[name].js',
+            path: path.resolve(__dirname, isProduction ? 'server/dist' : 'server/devdist')
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.tsx?$/,
+                    use: 'ts-loader',
+                    exclude: /node_modules/,
+                },
+                {
+                    test: /\.(jpe?g|gif|png|eot|woff|ttf|svg|woff2)$/,
+                    loader: 'url-loader?limit=10000'
+                }
+            ],
+        },
+        resolve: {
+            extensions: ['.tsx', '.ts', '.js'],
+        }
     }
-};
+}
