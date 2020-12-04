e3881938e07a75ea7f1c0df5a83561c58d7f9ebf
Jaiden Mispy <jaiden@mispy.me>
2020-12-04 23:20:08 +1100

    Link to mispy twitter for now

--- client/AppFooter.tsx
+++ client/AppFooter.tsx
@@ -8,7 +8,7 @@ export function AppFooter() {
                     <a href="https://github.com/mispy/dawnguide">GitHub</a>
                 </li>
                 <li>
-                    <a href="https://twitter.com/DawnguideApp">Twitter</a>
+                    <a href="https://twitter.com/mispy11">Twitter</a>
                 </li>
                 <li>
                     <a href="/contact">Contact</a>
