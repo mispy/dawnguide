887577ee25d933d757c41eb716429b0ddef32662
Jaiden Mispy <jaiden@mispy.me>
2020-12-06 23:00:56 +1100

    Be consistent about where the review button is

--- server/LessonPage.tsx
+++ server/LessonPage.tsx
@@ -9,9 +9,13 @@ import Markdown from "markdown-to-jsx"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faArrowRight } from "@fortawesome/free-solid-svg-icons"
 import { renderToStaticMarkup } from "react-dom/server"
+import classNames from 'classnames'
+import { Bibliography, transformRefs } from '../common/Bibliography'
 
 export function LessonPage(props: { lesson: Lesson }) {
     const { lesson } = props
+    const [lessonText, referenceIds] = transformRefs(lesson.text)
+    const referencesInText = referenceIds.map(id => lesson.expectReference(id))
 
     const intro = renderToStaticMarkup(<Markdown>{lesson.text.replace(/\[@[^\]]+\]/g, '')}</Markdown>)
     const pageDesc = lesson.summaryLine || htmlToPlaintext(intro).split("\n\n")[0]
@@ -25,13 +29,36 @@ export function LessonPage(props: { lesson: Lesson }) {
             <SiteHeader />
             <main className="LessonPage">
                 <div className="container">
-                    <Passage lesson={lesson} />
-                    <section>
-                        <div className="text-right">
-                            {lesson.type === 'reading' && <a href={`/review/${lesson.slug}`} className="btn btn-dawn">Review {lesson.title.toLowerCase()} <FontAwesomeIcon icon={faArrowRight} /></a>}
-                            {lesson.type === 'meditation' && <a href={`/login?then=${encodeURIComponent(`/${lesson.slug}`)}`} className="btn btn-dawn">Log in to try {lesson.name.toLowerCase()} <FontAwesomeIcon icon={faArrowRight} /></a>}
+                    <div className="Passage">
+                        <h1>
+                            {lesson.title}
+                        </h1>
+                        <Markdown>{lessonText}</Markdown>
+                        {'steps' in lesson.def ? <section id="steps">
+                            <Markdown>{lesson.def.steps}</Markdown>
+                        </section> : undefined}
+                        <div className="authorship">
+                            Written by {lesson.author}
                         </div>
-                    </section>
+                        <section>
+                            <div className="text-right">
+                                {lesson.type === 'reading' && <a href={`/review/${lesson.slug}`} className="btn btn-dawn">Review {lesson.title.toLowerCase()} <FontAwesomeIcon icon={faArrowRight} /></a>}
+                                {lesson.type === 'meditation' && <a href={`/login?then=${encodeURIComponent(`/${lesson.slug}`)}`} className="btn btn-dawn">Log in to try {lesson.name.toLowerCase()} <FontAwesomeIcon icon={faArrowRight} /></a>}
+                            </div>
+                        </section>
+                        {lesson.furtherReading ? <section id="furtherReading">
+                            <h2>Further Reading</h2>
+                            <Markdown>{lesson.furtherReading}</Markdown>
+                        </section> : undefined}
+                        {lesson.notes ? <section id="notes">
+                            <h2>Notes</h2>
+                            <Markdown>{lesson.notes}</Markdown>
+                        </section> : undefined}
+                        {referencesInText.length ? <section id="references">
+                            <h2>References</h2>
+                            <Bibliography references={referencesInText} />
+                        </section> : undefined}
+                    </div>
                 </div>
                 <SiteFooter />
             </main>
