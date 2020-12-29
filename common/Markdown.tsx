f651b785e26687583724473960156370736f3a02
Jaiden Mispy <jaiden@mispy.me>
2020-12-29 22:31:58 +1100

    Add bootstrap table class to gfm markdown tables

--- common/Markdown.tsx
+++ common/Markdown.tsx
@@ -2,11 +2,16 @@ import * as React from 'react'
 import LibMarkdown, { MarkdownOptions } from 'markdown-to-jsx'
 import { SmartLink } from './Passage'
 
+function Table(props: any) {
+    return <table className="table">{props.children}</table>
+}
+
 /** Wrap markdown-to-jsx with Dawnguide defaults */
 export function Markdown(props: { options?: MarkdownOptions, overrides?: any, children: any }) {
     const options = Object.assign({
         overrides: Object.assign({
-            a: SmartLink
+            a: SmartLink,
+            table: Table
         }, props.overrides)
     }, props.options)
     return <LibMarkdown options={options}>{props.children}</LibMarkdown>
