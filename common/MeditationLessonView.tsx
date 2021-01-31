e6eb2c0d0a895ccc41d8c4663afd032cb3da727d
Jaiden Mispy <jaiden@mispy.me>
2021-01-31 22:49:49 +1100

    Correctly observe interactivity on meditation timer

--- common/MeditationLessonView.tsx
+++ common/MeditationLessonView.tsx
@@ -4,22 +4,22 @@ import * as _ from 'lodash'
 /// #if CLIENT
 import { MeditationTimer } from '../client/MeditationTimer'
 /// #endif
-import type { Lesson, MeditationLesson } from "../common/content"
+import type { MeditationLesson } from "../common/content"
 import { Bibliography, transformRefs } from "../common/Bibliography"
 import { Markdown } from '../common/Markdown'
-import { Link } from 'react-router-dom'
 import { Container } from 'react-bootstrap'
 import { Observer } from 'mobx-react-lite'
 import { useProgressiveEnhancement } from './ProgressiveEnhancement'
 
 export function MeditationLessonView(props: { lesson: MeditationLesson }) {
-    const { js } = useProgressiveEnhancement()
 
     const { lesson } = props
     const [text, referenceIds] = transformRefs(lesson.def.text)
     const referencesInText = referenceIds.map(id => lesson.expectReference(id))
 
     return <Observer>{() => {
+        const { js } = useProgressiveEnhancement()
+
         return <Container>
             <h1>Meditation: {lesson.title}</h1>
             <Markdown>{text}</Markdown>
